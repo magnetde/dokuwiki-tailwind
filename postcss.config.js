544f5fe2dbe01333af186f5574160c440376166e
Fabian Röthlinger <magnetde@googlemail.com>
2023-04-20 18:36:14 +0000

    #8 - Style the navbar
    - Install plugins
    - Change logo
    - Add Navbar style

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 // postcss.config.js
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+    parser: require('postcss-comment'),
+    plugins: {
+        'postcss-import': {},
+        'tailwindcss/nesting': {},
+        tailwindcss: {},
+        autoprefixer: {},
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.php"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './**/*.php',
+        './node_modules/flowbite/**/*.js',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin'),
+        backdropBlur,
+    ],
 }
 
+function backdropBlur({ addVariant }) {
+    addVariant(
+        'supports-backdrop-blur',
+        '@supports (backdrop-filter: blur(0)) or (-webkit-backdrop-filter: blur(0))',
+    );
+}
