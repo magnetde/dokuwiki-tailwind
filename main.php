f0a06203c089f80702917d38489f8c1dc12f026e
Fabian Röthlinger <magnetde@googlemail.com>
2023-04-20 16:18:52 +0000

    #7 Template initialisieren
    - Style ändern

--- main.php
+++ main.php
@@ -21,21 +21,24 @@ $showSidebar = page_findnearest($conf['sidebar']) && ($ACT == 'show');
 <html lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>">
 	<head>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 		<link rel="apple-touch-icon" sizes="64x64" href="<?php echo tpl_basedir(); ?>images/apple-touch-icon.png">
 		<link rel="icon" type="image/x-icon" href="<?php echo tpl_basedir(); ?>images/favicon.ico">
 		<title>
 			<?php tpl_pagetitle()?> - <?php echo strip_tags($conf['title']) ?>
 		</title>
 		<?php
-        tpl_metaheaders();
-        echo tpl_favicon(array(
+		tpl_metaheaders();
+		echo tpl_favicon(array(
 			'favicon',
 			'mobile',
 		))
 		?>
 
 		<?php tpl_includeFile('meta.html')?>
+
+		<!-- CSS Files -->
+		<link href="<?php echo tpl_basedir(); ?>assets/css/main.css" rel="stylesheet" />
 
 		<!--- TODO: add styles and scripts --->
 	</head>
