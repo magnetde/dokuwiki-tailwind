c5601c4e39cccc268fae314974415cbefe17edf3
Fabian Röthlinger <magnetde@googlemail.com>
2023-04-24 00:04:24 +0000

    #16 - Edit the main content design
    - Add icons to inter wiki links

--- inc/EventHandlers.php
+++ inc/EventHandlers.php
@@ -44,30 +44,30 @@ class EventHandlers {
 		$edit_icon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">'
 		.'<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />'
 		.'</svg>';
-	
+
 		// Iterate over all edit button forms
 		foreach($html->find('.btn_secedit') as $elm) {
 			// Add the icon to the button
 			$btn = $elm->find('button', 0);
 			$btn->innertext = $edit_icon;
-	
+
 			if(!$lang['media_edit'])
 				continue;
-	
+
 			// determine the id of the edit button
 			$elm_id = $elm->find('input[name="hid"]', 0);
 			if(!$elm_id)
 				continue;
-	
+
 			$id = 'edit-button-' . $elm_id->value;
-	
+
 			// Add a new tooltip element
-	
+
 			$btn->setAttribute('data-tooltip-target', $id);
 			$btn->setAttribute('data-tooltip-placement', 'bottom');
-	
+
 			$label = sprintf($lang['media_edit'], $lang['doublequoteopening'] . $btn->title . $lang['doublequoteclosing']);
-	
+
 			$tooltip = '<div id="' . $id . '" role="tooltip" class="'
 				.clsx("
 					absolute z-10 inline-block px-3 py-2
@@ -79,7 +79,7 @@ class EventHandlers {
 				.'">' . $label
 				.'<div class="tooltip-arrow" data-popper-arrow></div>'
 				.'</div>';
-	
+
 			// Add the tooltip element
 			$elm->find('.no', 0)->innertext .= $tooltip;
 		}
@@ -96,43 +96,46 @@ class EventHandlers {
 	public function modifyContent($content) {
 		// FIX :-\ smile
 		$content = str_replace(['alt=":-\"', "alt=':-\'"], 'alt=":-&#92;"', $content);
-	
+
 		// Return original content if Simple HTML DOM fail or exceeded page size (default MAX_FILE_SIZE => 600KB)
 		if(strlen($content) > MAX_FILE_SIZE) {
 			return $content;
 		}
-	
+
 		// Import HTML string
 		$html = new simple_html_dom;
 		$html->load($content, true, false);
-	
+
 		// Return original content if Simple HTML DOM fail or exceeded page size (default MAX_FILE_SIZE => 600KB)
 		if(!$html) {
 			return $content;
 		}
-	
-		// Add anchors to headings
-	
-		$headers = array('h1', 'h2', 'h3', 'h4', 'h5');
-	
-		foreach($headers as $header) {
-			foreach($html->find($header) as $elm) {
-				$elm->addClass('group');
-	
-				$class = clsx("
-					ml-3 no-underline opacity-0 transition-opacity group-hover:opacity-100
-					text-gray-300 hover:text-gray-400
-					dark:text-gray-500 dark:hover:text-gray-400
-				");
-	
-				$elm->innertext .= '<a class="' . $class . '" href="#' . $elm->id . '">#</a>';
-			}
-		}
-	
+
+		$this->modifyHeaders($html);
+
 		$content = $html->save();
 		$html->clear();
 		unset($html);
 
 		return $content;
+	}
+
+	// Add anchors to headings
+	private function modifyHeaders($html) {
+		$headers = array('h1', 'h2', 'h3', 'h4', 'h5');
+
+		foreach($headers as $header) {
+			foreach($html->find($header) as $elm) {
+				$elm->addClass('group');
+
+				$class = clsx("
+					ml-3 no-underline opacity-0 transition-opacity group-hover:opacity-100
+					text-gray-300 hover:text-gray-400
+					dark:text-gray-500 dark:hover:text-gray-400
+				");
+
+				$elm->innertext .= '<a class="' . $class . '" href="#' . $elm->id . '">#</a>';
+			}
+		}
 	}
 }
