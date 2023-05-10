0d6027564c45c38fadbe8d93fa20bb10f9214caa
Fabian Röthlinger <magnetde@googlemail.com>
2023-05-10 14:08:15 +0000

    #49 - Fix design bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,22 @@ module.exports = {
     darkMode: 'media',
     theme: {
         extend: {
+            // make the navbar height constant
+            height: {
+                navbar: navbarHeight(),
+                content: contentHeight(),
+            },
+            minHeight: {
+                content: contentHeight(),
+            },
             // add a new default width to increase the content width
             maxWidth: {
                 '10xl': '104rem',
             },
+            // fix some border UI bugs on Safari
+            borderColor: {
+                DEFAULT: 'transparent',
+            }
         },
     },
     plugins: [
@@ -20,9 +32,17 @@ module.exports = {
     ],
 }
 
+function navbarHeight() {
+    return '4.5rem'
+}
+
+function contentHeight() {
+    return `calc(100vh - ${navbarHeight()} - 2px)`
+}
+
 function backdropBlur({ addVariant }) {
     addVariant(
         'supports-backdrop-blur',
         '@supports (backdrop-filter: blur(0)) or (-webkit-backdrop-filter: blur(0))',
-    );
+    )
 }
