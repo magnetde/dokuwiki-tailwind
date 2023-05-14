12bc2e706df482c02dd4487367cef34224cd8b68
Fabian Röthlinger <magnetde@googlemail.com>
2023-05-14 01:17:47 +0000

    #39 - Refactor and simplify CSS
    - Change navbar element

--- tpl/navbar.php
+++ tpl/navbar.php
@@ -56,9 +56,8 @@
 				<li>
 					<!--- Tools button --->
 					<button data-dropdown-toggle="dropdown-tools" class="<?php echo clsx("
-						flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded
-						hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto
-						dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent
+						md:btn-link
+						flex items-center justify-between w-full
 					") ?>">
 						<?php echo $lang['tools'] ?> <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
