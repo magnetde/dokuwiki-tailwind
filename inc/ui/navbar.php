591552202d68b1fbfbc3bba219b724e1e7947834
Fabian Röthlinger <magnetde@googlemail.com>
2023-06-11 02:52:49 +0000

    #70 - Fix minor bugs

--- inc/ui/navbar.php
+++ inc/ui/navbar.php
@@ -91,14 +91,13 @@
 						</span>
 
 						<?php
-						$user = $_SERVER['REMOTE_USER'];
-
-						if(!empty($user)) {
-							$avatar_url = _tpl_getavatar($user, $INFO['userinfo']['mail'], $avatar_size);
+						if(array_key_exists('REMOTE_USER', $_SERVER)) {
+							$user = $_SERVER['REMOTE_USER'];
+							$avatar_url = _tpl_getavatar($user, $INFO['userinfo']['mail']);
 							echo '<img class="w-8 h-8 rounded-full" src="' . $avatar_url . '" alt="' . $user . '">';
 						} else {
-							echo '<div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">'
-								.'<svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">'
+							echo '<div class="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">'
+								.'<svg class="absolute w-8 h-8 text-gray-400 top-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">'
 								.'<path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd">'
 								.'</path></svg>';
 						}
