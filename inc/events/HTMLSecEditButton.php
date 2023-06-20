f5d1f1089ad1072ec659742f6071f741890cabb7
Fabian Röthlinger <magnetde@googlemail.com>
2023-06-20 09:54:19 +0000

    #79 - Change the appearance of the secedit button

--- inc/events/HTMLSecEditButton.php
+++ inc/events/HTMLSecEditButton.php
@@ -3,7 +3,7 @@
 use simple_html_dom\simple_html_dom;
 
 /**
- * Event handler that modifies the section edit buttons.
+ * Event handler that modifies the section edit buttons by adding tooltips.
  */
 class HTMLSecEditButton extends EventHandler {
 
@@ -21,15 +21,11 @@ class HTMLSecEditButton extends EventHandler {
 		$html = new simple_html_dom;
 		$html->load($event->result, true, false);
 
-		$edit_icon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">'
-		.'<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />'
-		.'</svg>';
-
 		// Iterate over all edit button forms
 		foreach($html->find('.btn_secedit') as $elm) {
 			// Add the icon to the button
 			$btn = $elm->find('button', 0);
-			$btn->innertext = $edit_icon;
+			$btn->innertext = '';
 
 			if(!$lang['media_edit'])
 				continue;
