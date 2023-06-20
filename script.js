22f55337dbdf83e84da1b9242dfbc9a571b99886
Fabian Röthlinger <magnetde@googlemail.com>
2023-06-21 00:31:46 +0200

    #81 - Add support for diagrams plugin
    - Refactor

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-/* DOKUWIKI:include_once js/toolbar.js */
-/* DOKUWIKI:include_once js/draft.js */
-/* DOKUWIKI:include_once js/search.js */
-/* DOKUWIKI:include_once js/media.js */
+if(JSINFO.ACT === 'edit') {
+    /* DOKUWIKI:include_once js/toolbar.js */
+    /* DOKUWIKI:include_once js/draft.js */
+}
+
+if(JSINFO.ACT === 'search') {
+    /* DOKUWIKI:include_once js/search.js */
+}
+
+if(JSINFO.ACT === 'media') {
+    /* DOKUWIKI:include_once js/media.js */
+}
